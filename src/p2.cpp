74fa9d45fb601cbb613a9de5bb34de4e6eb0c8cb
annchen <b13ink830@gmail.com>
2025-11-25 14:59:51 +0800

    p3

--- src/p2.cpp
+++ src/p2.cpp
@@ -7,8 +7,8 @@ int gcd(int a, int b){
     if(a==b){
         return a;}
     if(a>b){
-        return gcd(a - b, b);}
-    return gcd(a, b - a);
+        return gcd(a-b, b);}
+    return gcd(a, b-a);
 }
 
 int main(){
