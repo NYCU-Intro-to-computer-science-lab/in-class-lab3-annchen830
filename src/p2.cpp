4a168c405afd723d5ed73232204761caa1c052fe
annchen <b13ink830@gmail.com>
2025-11-25 14:37:09 +0800

    p2

--- src/p2.cpp
+++ src/p2.cpp
@@ -4,15 +4,17 @@
 using namespace std;
 
 int gcd(int a, int b){
-    if (a == b) return a;
-    if (a > b) return gcd(a - b, b);
+    if(a==b){
+        return a;}
+    if(a>b){
+        return gcd(a - b, b);}
     return gcd(a, b - a);
 }
 
 int main(){
     int x, y;
-    cin >> x >> y;
-    cout << gcd(x, y);
+    cin>>x>>y;
+    cout<<gcd(x, y);
 }
 
     
